7022469d9ac1feefb6453717751e203a26167b4c
Oscar <pintososcar@gmail.com>
2016-04-30 20:33:43 -0300

    controlador presupuesto y ventana presupuesto con todos los geters necesarios

--- src/presentacion/controlador/ControladorPresupuesto.java
+++ src/presentacion/controlador/ControladorPresupuesto.java
@@ -1,5 +1,32 @@
 package presentacion.controlador;
 
-public class ControladorPresupuesto {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import presentacion.vista.VentanaPresupuesto;
+
+public class ControladorPresupuesto implements ActionListener{
+	
+	private VentanaPresupuesto ventanaPresupuesto;
+	
+	
+	public ControladorPresupuesto( VentanaPresupuesto ventanaPresupuesto) {
+		
+		 this.ventanaPresupuesto = ventanaPresupuesto;
+		
+	}
+	
+	public void inicializar(){
+		
+		this.ventanaPresupuesto.setVisible(true);
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
