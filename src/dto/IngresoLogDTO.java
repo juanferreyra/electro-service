52458d303409a05e2f58cacdb6b3987700feb746
Juan Ferreyra <juangabrielferreyra93@gmail.com>
2016-05-01 18:50:25 -0300

    Agrego IngresoLogDTO y IngresoDAO

---
+++ src/dto/IngresoLogDTO.java
@@ -0,0 +1,61 @@
+package dto;
+
+import java.util.Date;
+
+public class IngresoLogDTO {
+	private int id;
+	private int idingreso;
+	private int idestado;
+	private Date fecha_creacion;
+	private int idusuario;
+	
+	public IngresoLogDTO(int id, int idingreso, int idestado, Date fecha_creacion, int idusuario) {
+		this.id = id;
+		this.idingreso = idingreso;
+		this.idestado = idestado;
+		this.fecha_creacion = fecha_creacion;
+		this.idusuario = idusuario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIdingreso() {
+		return idingreso;
+	}
+
+	public void setIdingreso(int idingreso) {
+		this.idingreso = idingreso;
+	}
+
+	public int getIdestado() {
+		return idestado;
+	}
+
+	public void setIdestado(int idestado) {
+		this.idestado = idestado;
+	}
+
+	public Date getFecha_creacion() {
+		return fecha_creacion;
+	}
+
+	public void setFecha_creacion(Date fecha_creacion) {
+		this.fecha_creacion = fecha_creacion;
+	}
+
+	public int getIdusuario() {
+		return idusuario;
+	}
+
+	public void setIdusuario(int idusuario) {
+		this.idusuario = idusuario;
+	}
+	
+	
+}
---
+++ src/persistencia/dao/IngresoLogDAO.java
@@ -0,0 +1,96 @@
+package persistencia.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.IngresoLogDTO;
+import persistencia.conexion.Conexion;
+
+public class IngresoLogDAO {
+	
+	private static final String insert = "INSERT INTO ingreso_log("
+			+ "`idingreso`, `idestado`, `fecha_creacion`, `idusuario`, `habilitado`)"
+			+ "VALUES (?,?, now(), ?, true);";
+	private static final String delete = "UPDATE ingreso_log SET habilitado='0' WHERE id = ?";
+	private static final String readall = "SELECT * FROM ingreso_log WHERE habilitado = true;";
+	private static final Conexion conexion = Conexion.getConexion();
+	
+	public boolean insert(IngresoLogDTO ingreso_log)
+	{
+		PreparedStatement statement;
+		try 
+		{
+			statement = conexion.getSQLConexion().prepareStatement(insert);
+			statement.setInt(1, ingreso_log.getIdingreso());
+			statement.setInt(2, ingreso_log.getIdestado());
+			statement.setInt(2, ingreso_log.getIdusuario());
+			
+			if(statement.executeUpdate() > 0) //Si se ejecut� devuelvo true
+				return true;
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		finally //Se ejecuta siempre
+		{
+			conexion.cerrarConexion();
+		}
+		return false;
+	}
+	
+	public boolean delete(IngresoLogDTO ingreso_a_eliminar)
+	{
+		PreparedStatement statement;
+		int chequeoUpdate=0;
+		try 
+		{
+			statement = conexion.getSQLConexion().prepareStatement(delete);
+			statement.setString(1, Integer.toString(ingreso_a_eliminar.getId()));
+			chequeoUpdate = statement.executeUpdate();
+			if(chequeoUpdate > 0) //Si se ejecut� devuelvo true
+				return true;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		finally //Se ejecuta siempre
+		{
+			conexion.cerrarConexion();
+		}
+		return false;
+	}
+	
+	public List<IngresoLogDTO> readAll(int idIngreso)
+	{
+		PreparedStatement statement;
+		ResultSet resultSet; //Guarda el resultado de la query
+		ArrayList<IngresoLogDTO> marcas = new ArrayList<IngresoLogDTO>();
+		try
+		{
+			statement = conexion.getSQLConexion().prepareStatement(readall);
+			resultSet = statement.executeQuery();
+			
+			while(resultSet.next())
+			{
+				marcas.add(new IngresoLogDTO(resultSet.getInt("id"),resultSet.getInt("idingreso"),resultSet.getInt("idestado"),resultSet.getDate("fecha_creacion"),resultSet.getInt("idusuario")));
+			}
+		}
+		catch (SQLException e) 
+		{
+			System.out.println("hubo un error");
+			e.printStackTrace();
+		}
+		finally //Se ejecuta siempre
+		{
+			conexion.cerrarConexion();
+		}
+		
+		return marcas;
+	}
+		
+}
