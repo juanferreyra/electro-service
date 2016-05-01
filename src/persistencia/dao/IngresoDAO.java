3e9a216a1bc90ab4e9ac3ad6f9b8c5410f8ebc7b
ssaczkows <ssaczkows@outlook.com>
2016-05-01 16:52:56 -0300

    Actualizaciones pantalla principal

--- src/persistencia/dao/IngresoDAO.java
+++ src/persistencia/dao/IngresoDAO.java
@@ -1,5 +1,17 @@
 package persistencia.dao;
 
+import java.util.ArrayList;
+
+import dto.IngresoDTO;
+
 public class IngresoDAO {
+	
+	
+	
+
+	public ArrayList<IngresoDTO> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
