ef9599b100979c94fe3983b91df820dc3e35a30f
Juan Ferreyra <juangabrielferreyra93@gmail.com>
2016-05-04 01:24:05 -0300

    agrego librerias faltantes de jasper y clase de reporte

---
+++ src/presentacion/reportes/ReporteIngreso.java
@@ -0,0 +1,48 @@
+package presentacion.reportes;
+
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import modelo.Ingreso;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class ReporteIngreso
+{
+	private JasperReport reporte;
+	private JasperViewer reporteViewer;
+	private JasperPrint	reporteLleno;
+	
+	//Recibe la lista de personas para armar el reporte
+    public ReporteIngreso(Ingreso ing)
+    {
+    	//Hardcodeado
+		Map<String, Object> parametersMap = new HashMap<String, Object>();
+		parametersMap.put("Fecha", new SimpleDateFormat("dd/MM/yyyy").format(new Date()));		
+    	try		{
+    		
+			this.reporte = (JasperReport) JRLoader.loadObjectFromFile( "ReporteIngreso.jasper" );
+			this.reporteLleno = JasperFillManager.fillReport(this.reporte, parametersMap, 
+					new JRBeanCollectionDataSource((Collection<?>) ing));
+		}
+		catch( JRException ex ) 
+		{
+			ex.printStackTrace();
+		}
+    }       
+    
+    public void mostrar()
+	{
+		this.reporteViewer = new JasperViewer(this.reporteLleno,false);
+		this.reporteViewer.setVisible(true);
+	}
+   
+}	
