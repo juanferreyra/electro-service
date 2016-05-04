0a64e7af38df962f26fbe96ae65a030375a21659
ssaczkows <ssaczkows@outlook.com>
2016-05-04 01:13:38 -0300

    Modifico Usuario DAO y modelo usuario

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,21 +1,14 @@
 package modelo;
 
-import java.util.List;
-
 import persistencia.dao.UsuarioDAO;
 
 public class Usuario {
 
 	private UsuarioDAO usuario;
-	
-	public Usuario(){
-		
+
+	public Usuario() {
+
 		this.usuario = new UsuarioDAO();
 	}
 
-	public List<String> buscarUsuario(int id) {
-		return usuario.find(id);
-	}
-	
-	
 }
