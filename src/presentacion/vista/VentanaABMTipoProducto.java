03836c85adb5144490d6ceeeb38bb6330c294c3c
ssaczkows <ssaczkows@outlook.com>
2016-05-25 14:31:03 -0300

    Subo vista ABM Tipo Producto

--- src/presentacion/vista/VentanaABMTipoProducto.java
+++ src/presentacion/vista/VentanaABMTipoProducto.java
@@ -1,12 +1,12 @@
 package presentacion.vista;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
@@ -17,37 +17,52 @@ public class VentanaABMTipoProducto extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField detalle_txt;
+	private JTable tablaTipoProducto;
+	private DefaultTableModel modelTipoProducto;
+	private String[] nombreColumnas = { "Detalle" };
 
 	public VentanaABMTipoProducto() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 658, 334);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		JButton cancelar_btn = new JButton("Cancelar");
-		cancelar_btn.setBounds(315, 228, 89, 23);
+		cancelar_btn.setBounds(500, 252, 89, 23);
 		getContentPane().add(cancelar_btn);
 
 		JButton guardar_btn = new JButton("Guardar");
-		guardar_btn.setBounds(216, 228, 89, 23);
+		guardar_btn.setBounds(401, 252, 89, 23);
 		getContentPane().add(guardar_btn);
-		
+
 		JLabel title_lbl = new JLabel("Nuevo tipo de producto");
 		title_lbl.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		title_lbl.setHorizontalAlignment(SwingConstants.CENTER);
-		title_lbl.setBounds(10, 11, 414, 14);
+		title_lbl.setBounds(10, 11, 622, 14);
 		contentPane.add(title_lbl);
-		
+
 		JLabel detalle_lbl = new JLabel("Detalle");
-		detalle_lbl.setBounds(42, 76, 76, 14);
+		detalle_lbl.setBounds(83, 76, 76, 14);
 		contentPane.add(detalle_lbl);
-		
+
 		detalle_txt = new JTextField();
-		detalle_txt.setBounds(104, 73, 300, 20);
+		detalle_txt.setBounds(128, 73, 455, 20);
 		contentPane.add(detalle_txt);
 		detalle_txt.setColumns(10);
+
+		JScrollPane tipoProducto_scrollPane = new JScrollPane();
+		tipoProducto_scrollPane.setBounds(83, 135, 504, 91);
+		getContentPane().add(tipoProducto_scrollPane);
+
+		modelTipoProducto = new DefaultTableModel(null, nombreColumnas);
+		tablaTipoProducto = new JTable(modelTipoProducto);
+		tipoProducto_scrollPane.setViewportView(tablaTipoProducto);
+
+		JButton eliminarItem_btn = new JButton("Eliminar Item");
+		eliminarItem_btn.setBounds(269, 252, 121, 23);
+		getContentPane().add(eliminarItem_btn);
 	}
 
 }
