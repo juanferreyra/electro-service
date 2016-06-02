301357940dfc8b587b9af72f4be5b52b7130cbb4
ssaczkows <ssaczkows@outlook.com>
2016-06-02 01:31:25 -0300

    modifico ventana tipoProducto

--- src/presentacion/vista/VentanaABMTipoProducto.java
+++ src/presentacion/vista/VentanaABMTipoProducto.java
@@ -25,7 +25,7 @@ public class VentanaABMTipoProducto extends JFrame {
 	private JButton limpiar_btn;
 
 	public VentanaABMTipoProducto() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 658, 334);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
