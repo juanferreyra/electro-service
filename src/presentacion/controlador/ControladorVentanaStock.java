d25e9f0670f46d0c52ead46d53d78ca3b1637190
ssaczkows <ssaczkows@outlook.com>
2016-06-04 12:42:55 -0300

    Agrego pantalla controlador y formato tabla STOCK

---
+++ src/presentacion/controlador/ControladorVentanaStock.java
@@ -0,0 +1,87 @@
+package presentacion.controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.table.DefaultTableModel;
+
+import dto.UsuarioDTO;
+import presentacion.vista.VentanaOrdenCompra;
+import presentacion.vista.VentanaStock;
+
+public class ControladorVentanaStock implements ActionListener {
+
+	private VentanaStock ventanaStock;
+
+	public ControladorVentanaStock(VentanaStock ventanaStock) {
+		this.ventanaStock = ventanaStock;
+		this.ventanaStock.getGenerarOC_btn().addActionListener(this);
+
+	}
+
+	public void inicializar() {
+		// Crear todos los news de los datos
+		this.cargar_tablaStock();
+	}
+
+	private void cargar_tablaStock() {
+		// Consigo todos los datos de stock y genero las filas
+		// ArrayList<datosStock> datosStock = datosStockDAO.readAll();
+		// ObtenerFilas(datos);
+		//
+
+	}
+
+	// private void ObtenerFilas(ArrayList<datosStock> datos) {
+	// limpiar_tablaStock();
+	// for (int i = 0; i <= datos.size() - 1; i++) {
+	// this.cargarFila(i, marca, repuesto, existencia, reservado, pedido,
+	// disponible);
+	// }
+	// }
+
+	private void cargarFila(int fila, String marca, String repuesto, int existencia, int reservado, int pedido,
+			int disponible) {
+
+		Object[] dato = new Object[6];
+		dato[0] = marca;
+		dato[1] = repuesto;
+		dato[2] = existencia;
+		dato[3] = reservado;
+		dato[4] = pedido;
+		dato[5] = disponible;
+
+		// Establezco que no se pueda editar pero si seleccionar una fila
+		((DefaultTableModel) this.ventanaStock.getModelo()).isCellEditable(fila, 0);
+		((DefaultTableModel) this.ventanaStock.getModelo()).isCellEditable(fila, 1);
+		((DefaultTableModel) this.ventanaStock.getModelo()).isCellEditable(fila, 2);
+		((DefaultTableModel) this.ventanaStock.getModelo()).isCellEditable(fila, 3);
+		((DefaultTableModel) this.ventanaStock.getModelo()).isCellEditable(fila, 4);
+		((DefaultTableModel) this.ventanaStock.getModelo()).isCellEditable(fila, 5);
+
+	}
+
+	public void limpiar_tablaStock() {
+
+		int largo = ((DefaultTableModel) this.ventanaStock.getModelo()).getRowCount();
+
+		for (int i = largo - 1; i >= 0; i--) {
+			((DefaultTableModel) this.ventanaStock.getModelo()).removeRow(i);
+		}
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == this.ventanaStock.getGenerarOC_btn()) {
+			ControladorOrdenCompra controladorOC = new ControladorOrdenCompra(new VentanaOrdenCompra(),
+					new UsuarioDTO(0, null, null, null, 0));// Ingresar el
+															// usuario correcto
+															// !!!!!
+			controladorOC.inicializar();
+		}
+
+	}
+
+}
---
+++ src/presentacion/vista/FormatoTablaStock.java
@@ -0,0 +1,51 @@
+package presentacion.vista;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class FormatoTablaStock extends DefaultTableCellRenderer {
+
+	private static final long serialVersionUID = 1L;
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean selected, boolean focused,
+			int row, int column) {
+
+		// Formato de colores de filas segun estado.
+		aplicarColorFilas(table, value, selected, focused, row, column);
+
+		super.getTableCellRendererComponent(table, value, selected, focused, row, column);
+		return this;
+	}
+
+	private void aplicarColorFilas(JTable table, Object value, boolean selected, boolean focused, int row, int column) {
+		// if (String.valueOf(table.getValueAt(row, 3)).equals("NUEVO")) {
+		setBackground(Color.decode("#D8F6CE"));
+		// }
+		// else if (String.valueOf(table.getValueAt(row,
+		// 8)).equals("PRESUPUESTADO")) {
+		// setBackground(Color.decode("#F6E3CE"));
+		// } else if (String.valueOf(table.getValueAt(row,
+		// 8)).equals("RETIRADO")) {
+		// setBackground(Color.decode("#F5A9A9"));
+		// } else if (String.valueOf(table.getValueAt(row,
+		// 8)).equals("REPARADO")) {
+		// setBackground(Color.decode("#CEF6F5"));
+		// } else if (String.valueOf(table.getValueAt(row,
+		// 8)).equals("IRREPARABLE")) {
+		// setBackground(Color.decode("#CED8F6"));
+		// } else if (String.valueOf(table.getValueAt(row,
+		// 8)).equals("INFORMADO")) {
+		// setBackground(Color.decode("#F5F6CE"));
+		// }else if (String.valueOf(table.getValueAt(row,
+		// 8)).equals("ACEPTADO")) {
+		// setBackground(Color.decode("#CEF6E3"));
+		// }else if (String.valueOf(table.getValueAt(row,
+		// 8)).equals("RECHAZADO")) {
+		// setBackground(Color.decode("#F6D8CE"));
+		// }
+	}
+
+}
