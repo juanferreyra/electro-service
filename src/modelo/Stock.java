75279c22f7b70ef6971925a9fe4157649af7497f
ssaczkows <ssaczkows@outlook.com>
2016-06-06 22:18:59 -0300

    agrego una funcion en stock, y creo 3 metodos en controlador principal para aviso faltante ademas agrego colores al formato de la tabla

--- src/modelo/Stock.java
+++ src/modelo/Stock.java
@@ -10,14 +10,14 @@ import persistencia.dao.RepuestoDAO;
 
 public class Stock {
 
-private static ItemStockDAO itemStockDAO;
-	
-	public Stock (){
-		
+	private static ItemStockDAO itemStockDAO;
+
+	public Stock() {
+
 		Stock.itemStockDAO = new ItemStockDAO();
-		
+
 	}
-	
+
 	public void agregarRepuesto(RepuestoDTO nuevoComponente) {
 		itemStockDAO.insert(nuevoComponente);
 	}
@@ -25,39 +25,41 @@ private static ItemStockDAO itemStockDAO;
 	public void borrarRepuestoDelStock(int id_componente_a_eliminar) {
 		itemStockDAO.delete(id_componente_a_eliminar);
 	}
-	
-	public boolean modificarStock (List<InsumoStockDTO> insumosGastados ){
-	
-		
+
+	public boolean modificarStock(List<InsumoStockDTO> insumosGastados) {
+
 		return true;
-	}; 
-	
+	};
+
 	public ArrayList<InsumoStockDTO> levantarStock() {
-		
-		return itemStockDAO.readAll();		
+
+		return itemStockDAO.readAll();
 	}
-	
-	public void  modificarStock(long repuestoID, int variacion) {
+
+	public void modificarStock(long repuestoID, int variacion) {
 		itemStockDAO.update(repuestoID, variacion);
 	}
-	
-	public  ArrayList<InsumoStockDTO> cargarPedidos(ArrayList<InsumoStockDTO> insumosStock ) {
-		
-		return itemStockDAO.cargarInsumosPedidos(insumosStock);		
+
+	public ArrayList<InsumoStockDTO> cargarPedidos(ArrayList<InsumoStockDTO> insumosStock) {
+
+		return itemStockDAO.cargarInsumosPedidos(insumosStock);
 	}
-		
+
 	public ArrayList<InsumoStockDTO> obtenerStock() {
-		ArrayList<InsumoStockDTO> stock = new  ArrayList<InsumoStockDTO>();
+		ArrayList<InsumoStockDTO> stock = new ArrayList<InsumoStockDTO>();
 		stock = itemStockDAO.readAll();
 		System.out.println(stock.size());
 		stock = cargarReservados(stock);
 		stock = cargarPedidos(stock);
-		
-		
+
 		return stock;
 	}
-	
-	public ArrayList<InsumoStockDTO> cargarReservados(ArrayList<InsumoStockDTO> insumosStock ){
-	return itemStockDAO.CargarInsumosReservados(insumosStock);	
+
+	public ArrayList<InsumoStockDTO> cargarReservados(ArrayList<InsumoStockDTO> insumosStock) {
+		return itemStockDAO.CargarInsumosReservados(insumosStock);
+	}
+
+	public ArrayList<InsumoStockDTO> getFaltante() {
+		return cargarPedidos(obtenerStock());
 	}
 }
